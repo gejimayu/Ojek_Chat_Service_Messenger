4c779691217652c8e7e5e5d96f452e3728ece7cd
patricknugrohoh <patrick.nugroho64@yahoo.com>
2017-11-25 17:04:24 +0700

    OjekDataPublisher.java fixed

--- OjekService/src/org/java/ojekonline/webservice/OjekDataPublisher.java
+++ OjekService/src/org/java/ojekonline/webservice/OjekDataPublisher.java
@@ -3,6 +3,6 @@ import javax.xml.ws.Endpoint;
 
 public class OjekDataPublisher {
 	public static void main(String[] args) {
-		  Endpoint.publish("http://localhost:8082/WS/OjekData",new OjekDataImpl());
+		  Endpoint.publish("http://localhost:8082/WS/OjekData",new OjekDataImpl()); //initiate new object of OjekDataImpl
 	}
 }
